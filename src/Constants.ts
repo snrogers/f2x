bb5133cd5b0eb3b8e62cc55f6cfdc0393cb2890e
Sebastian Rogers <sebastian.n.rogers@gmail.com>
2024-11-23 13:39:59 -0500

    Whitelist css and scss

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,10 +1,12 @@
 export const ALLOWED_EXTENSIONS = [
+  '.css',
   '.js',
   '.jsx',
   '.json',
   '.html',
   '.html.in', // Something weird for zig docs
   '.md',
+  '.scss',
   '.ts',
   '.tsx',
   '.yml',
